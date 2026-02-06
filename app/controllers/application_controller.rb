9cffe69caf13da792b8dfbc0e7debda833d76d1e
LucioAlec <lucioalecpro@gmail.com>
2026-02-06 01:22:01 -0300

    Redirect back to intended URL after sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def require_signin
     unless current_user
-    session[:intended_url] == request.url
+    session[:intended_url] = request.url
       redirect_to new_session_url, alert: "Please, you must to sign in first!"
     end
   end
