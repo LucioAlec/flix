818cc8e2cd6a6991dac1e1f0de79868f74cae9e8
LucioAlec <lucioalecpro@gmail.com>
2026-02-25 13:50:20 -0300

    Fix duplicate aws-sdk-s3 gem entry

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -48,7 +48,7 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie)
       .permit(:title, :description, :rating,
-               :director, :duration, :image_file_name, :released_on, :total_gross, genre_ids: [])
+               :director, :duration, :main_image, :released_on, :total_gross, genre_ids: [])
   end
 
   def set_movie
---
+++ db/migrate/20260224205604_remove_the_image_file_name_from_movies.rb
@@ -0,0 +1,5 @@
+class RemoveTheImageFileNameFromMovies < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :movies, :image_file_name, :string
+  end
+end
