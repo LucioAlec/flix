ee8f58c282c366871a7394c6dcb13f6ffa678e77
LucioAlec <lucioalecpro@gmail.com>
2026-02-28 11:59:12 -0300

    Fix identation

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   resources :genres
+
   root "movies#index"
+
   get "movies/filter/:filter" => "movies#index", as: :filtered_movies
+
   resources :movies do
     resources :reviews
     resources :favorites, only: [ :create, :destroy ]
   end
-=begin
-  get "movies" => "movies#index"
-  get "movies/new" => "movies#new"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit"=> "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
-=end
-resource :session, only: [ :new, :create, :destroy ]
-resources :users
-get "signin" =>"sessions#new"
-get "signup" =>"users#new"
+
+  resource :session, only: [ :new, :create, :destroy ]
+
+  resources :users
+
+  get "signin" =>"sessions#new"
+  
+  get "signup" =>"users#new"
 end
